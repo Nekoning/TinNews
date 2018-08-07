2fb87d4380013a412710062bbf487ae21f875e9e
Nekoning <nekoningwang@gmail.com>
2018-08-07 14:50:00 -0400

    First upload

---
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -0,0 +1,37 @@
+package com.laioffer.tinnews;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+import com.laioffer.tinnews.common.TinBasicActivity;
+import com.laioffer.tinnews.common.TinFragmentPagerAdapter;
+
+public class MainActivity extends TinBasicActivity {
+
+    private ViewPager viewPager;
+    private BottomNavigationView bottomBar;
+    private TinFragmentPagerAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //link view pager with view
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new TinFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(TinFragmentPagerAdapter.FRAGMENT_NUMBER);
+    }
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void showSnackBar(String message) {
+
+    }
+}
