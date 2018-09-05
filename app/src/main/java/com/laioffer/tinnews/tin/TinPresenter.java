e18241563c6a3857221b1be410eca952db456b02
Nekoning <nekoningwang@gmail.com>
2018-09-04 23:33:19 -0400

    implement change country - eventbus

--- app/src/main/java/com/laioffer/tinnews/tin/TinPresenter.java
+++ app/src/main/java/com/laioffer/tinnews/tin/TinPresenter.java
@@ -1,6 +1,11 @@
 package com.laioffer.tinnews.tin;
 
+import com.laioffer.tinnews.Profile.CountryEvent;
 import com.laioffer.tinnews.retrofit.response.News;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
 
@@ -8,6 +13,7 @@ public class TinPresenter implements TinContract.Presenter {
 
     private TinContract.View view;
     private TinContract.Model model;
+    private static final String DEFAULT_COUNTRY = "us";
 
     public TinPresenter() {
         this.model = new TinModel();
@@ -16,18 +22,26 @@ public class TinPresenter implements TinContract.Presenter {
 
     @Override
     public void onCreate() {
-
+        EventBus.getDefault().register(this);
     }
 
     @Override
     public void onDestroy() {
-
+        EventBus.getDefault().unregister(this);
     }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onEvent(CountryEvent countryEvent) {
+        if (this.view != null) {
+            this.model.fetchData(countryEvent.country);
+        }
+    }
+
 
     @Override
     public void onViewAttached(TinContract.View view) {
         this.view = view;
-        this.model.fetchData();
+        this.model.fetchData(DEFAULT_COUNTRY);
     }
 
     @Override
